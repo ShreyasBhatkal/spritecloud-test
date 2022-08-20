088b92382b26bd762bf7c07e388c73e8cbc9db2b
ShreyasBhatkal <shreyasbhatkal26@gmail.com>
2022-08-20 12:59:11 +0200

    Add files

---
+++ src/ui_automate.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import time
+
+
+
+def main():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_ui_automate.py
@@ -0,0 +1,5 @@
+from ui_automate import main
+
+
+def test_ui_automate():
+    assert main() == "Hello World"
